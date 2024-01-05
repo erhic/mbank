e83e5c87b7101eec7bf9e519fee4215fcb30ab58
erhic <ericngugi24@gmail.com>
2024-01-05 23:56:02 +0300

    update register endpoint url

--- Pages/Register.js
+++ Pages/Register.js
@@ -8,9 +8,54 @@ import {
   TouchableOpacity,
   Image,
 } from "react-native";
-import { COLORS } from "../assets/styles/Utils";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { COLORS, REGISTERSTYLES } from "../assets/styles/Utils";
 
 export default function Register({ navigation }) {
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [accountno, setAccountNo] = useState("");
+  const [pin, setPin] = useState("");
+
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const userHandler = (text) => {
+    setFirstName(text);
+  };
+
+  const userLstnameHandler = (text) => {
+    setLastName(text);
+  };
+  const accountHandler = (text) => {
+    setAccountNo(text);
+  };
+  const pinHandler = (text) => {
+    setPin(text);
+  };
+
+  useEffect(() => {
+    const authenticate = (async) => {
+      axios
+        .post(
+          "https://mb4nk.000webhostapp.com/register.php",
+          JSON.stringify({
+            firstname: firstname,
+            lastname: lastname,
+            accountno: accountno,
+            pin: pin,
+          })
+        )
+        .then((response) => {
+          console.log(response);
+          setIsSubmit(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    if (isSubmit) authenticate();
+  }, [isSubmit]);
   return (
     <SafeAreaView>
       <View
@@ -27,23 +72,21 @@ export default function Register({ navigation }) {
         <Text style={styles.registerLabelthree}>First Name</Text>
         <TextInput
           style={styles.input}
-          // onChangeText={onChangeText}
-          // placeholder="Enter first name"
-          // placeholderTextColor={(style = { color: COLORS.lightWhite })}
+          onChangeText={userHandler}
+          autoCapitalize="none"
         />
         <Text style={styles.registerLabelFour}>Last Name</Text>
         <TextInput
           style={styles.input}
-          // onChangeText={onChangeText}
-          // placeholder="Enter last name"
+          onChangeText={userLstnameHandler}
           placeholderTextColor={(style = { color: COLORS.lightWhite })}
         />
         <Text style={styles.registerLabelOne}>Account No</Text>
         <TextInput
           style={styles.input}
           keyboardType="numeric"
-          // onChangeText={onChangeText}
-          // placeholder="Enter account number"
+          autoCapitalize="none"
+          onChangeText={accountHandler}
           placeholderTextColor={(style = { color: COLORS.lightWhite })}
         />
       </View>
@@ -51,32 +94,19 @@ export default function Register({ navigation }) {
         <Text style={styles.registerLabelTwo}>PIN</Text>
         <TextInput
           style={styles.input}
-          // onChangeText={onChangePin}
+          autoCapitalize="none"
+          onChangeText={pinHandler}
           placeholder="********"
           placeholderTextColor={(style = { color: COLORS.lightWhite })}
         />
       </View>
-      <TouchableOpacity style={styles.registerBtn}>
-        <Text
-          style={{
-            backgroundColor: COLORS.primary,
-            color: COLORS.lightWhite,
-            paddingHorizontal: 40,
-            paddingVertical: 10,
-            borderRadius: 10,
-          }}
-        >
-          Register
-        </Text>
-      </TouchableOpacity>
-      <View
-        style={{
-          alignSelf: "center",
-          display: "flex",
-          flexDirection: "row",
-          paddingVertical: 15,
-        }}
+      <TouchableOpacity
+        style={styles.registerBtn}
+        onPress={() => setIsSubmit(true)}
       >
+        <Text style={styles.btnText}>Register</Text>
+      </TouchableOpacity>
+      <View style={styles.btnContainer}>
         <Text>Already have an account ?</Text>
         <TouchableOpacity onPress={() => navigation.navigate("Login")}>
           <Text style={{ paddingLeft: 10 }}>Login</Text>
@@ -87,55 +117,13 @@ export default function Register({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  registerLabelOne: {
-    // textAlign: "center",
-    paddingLeft: 60,
-    paddingTop: 10,
-    color: COLORS.secondary,
-    paddingBottom: 10,
-  },
-  registerLabelTwo: {
-    // textAlign: "center",
-    paddingLeft: 60,
-    color: COLORS.secondary,
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
-  input: {
-    height: 40,
-    marginHorizontal: 55,
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 10,
-    backgroundColor: COLORS.lightWhite,
-  },
-  registerBtn: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 44,
-  },
-  logoImg: {
-    backgroundColor: COLORS.primary,
-    alignSelf: "center",
-    display: "flex",
-
-    width: 100,
-    objectFit: "contain",
-  },
-  registerLabelthree: {
-    // textAlign: "center",
-    paddingLeft: 60,
-    color: COLORS.secondary,
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
-  registerLabelFour: {
-    // textAlign: "center",
-    paddingLeft: 60,
-    color: COLORS.secondary,
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
+  registerLabelOne: REGISTERSTYLES.registerLabelOne,
+  registerLabelTwo: REGISTERSTYLES.registerLabelTwo,
+  input: REGISTERSTYLES.input,
+  registerBtn: REGISTERSTYLES.registerBtn,
+  logoImg: REGISTERSTYLES.logoImg,
+  registerLabelthree: REGISTERSTYLES.registerLabelthree,
+  registerLabelFour: REGISTERSTYLES.registerLabelFour,
+  btnContainer: REGISTERSTYLES.btnContainer,
+  btnText: REGISTERSTYLES.btnText,
 });
--- assets/styles/Utils.js
+++ assets/styles/Utils.js
@@ -56,6 +56,7 @@ const HEADER = {
     backgroundColor: COLORS.primary,
     color: COLORS.lightWhite,
     paddingBottom: 15,
+
     fontWeight: "bold",
   },
   backBtn: {
@@ -67,5 +68,138 @@ const HEADER = {
     height: 20,
   },
 };
-
-export { COLORS, FONT, SIZES, SHADOWS, HEADER };
+const REGISTERSTYLES = {
+  registerLabelOne: {
+    paddingLeft: 60,
+    paddingTop: 5,
+    color: COLORS.secondary,
+    paddingBottom: 10,
+  },
+  registerLabelTwo: {
+    paddingLeft: 60,
+    color: COLORS.secondary,
+    paddingTop: 5,
+    paddingBottom: 10,
+  },
+  input: {
+    height: 40,
+    marginHorizontal: 55,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: COLORS.lightWhite,
+  },
+  registerBtn: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 27,
+  },
+  logoImg: {
+    backgroundColor: COLORS.primary,
+    alignSelf: "center",
+    display: "flex",
+    marginTop: -45,
+    width: 100,
+    objectFit: "contain",
+  },
+  registerLabelthree: {
+    paddingLeft: 60,
+    color: COLORS.secondary,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  registerLabelFour: {
+    paddingLeft: 60,
+    color: COLORS.secondary,
+    paddingTop: 5,
+    paddingBottom: 10,
+  },
+  btnContainer: {
+    alignSelf: "center",
+    display: "flex",
+    flexDirection: "row",
+    paddingVertical: 15,
+  },
+  btnText: {
+    backgroundColor: COLORS.primary,
+    color: COLORS.lightWhite,
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+};
+const LOGINSTYLES = {
+  loginLabelOne: {
+    paddingLeft: 60,
+    paddingTop: 45,
+    color: COLORS.secondary,
+    paddingBottom: 10,
+  },
+  loginLabelTwo: {
+    paddingLeft: 60,
+    color: COLORS.secondary,
+    paddingTop: 15,
+    paddingBottom: 10,
+  },
+  input: {
+    height: 40,
+    marginHorizontal: 55,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: COLORS.lightWhite,
+  },
+  inputPass: {
+    height: 40,
+    marginLeft: 55,
+    borderWidth: 1,
+    padding: 10,
+    width: 269,
+    borderRadius: 10,
+    backgroundColor: COLORS.lightWhite,
+  },
+  loginBtn: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 35,
+  },
+  logoImg: {
+    backgroundColor: COLORS.primary,
+    alignSelf: "center",
+    display: "flex",
+    width: 100,
+    objectFit: "contain",
+  },
+  registerText: {
+    color: COLORS.secondary,
+    borderRadius: 10,
+  },
+  registerLink: {
+    color: COLORS.secondary,
+    paddingLeft: 10,
+  },
+  register: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 25,
+  },
+  eyeIcon: {
+    objectFit: "contain",
+    width: 30,
+    marginLeft: -40,
+  },
+  textBtn: {
+    backgroundColor: COLORS.primary,
+    color: COLORS.lightWhite,
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+};
+export { COLORS, FONT, SIZES, SHADOWS, HEADER, REGISTERSTYLES, LOGINSTYLES };
